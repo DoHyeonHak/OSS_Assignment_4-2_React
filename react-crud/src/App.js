dc497ac03dfad4f24151129e3213450da10d6ef5
Hyeonhak_Do <ehgusgkr6@naver.com>
2024-11-07 11:13:50 +0900

    create and edit react project

---
+++ react-crud/src/App.js
@@ -0,0 +1,20 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Hello from "./Hello.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/myCharacter.png" alt="My Character" />
+        <Hello />
+        <p>This is a React project created for assignment purposes.</p>
+      </header>
+      <section className="App-content">
+        <p>@csee_dhh</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-crud/src/Hello.js
@@ -0,0 +1,7 @@
+import "./Hello.css";
+
+function Hello(props) {
+  return <h1>This is Hyeonhak's First React page!</h1>;
+}
+
+export default Hello;
